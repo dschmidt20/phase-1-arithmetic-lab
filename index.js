0259e7caa3a4a7fed9ea728df9b4e202f99d761d
Daniel Schmidt <d.schmidt20@yahoo.com>
2021-11-22 15:31:41 -0800

    commit tree 726802bb56c9e4ff96b312967d6d3580333d6891

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let num3 = 19
+let num4 = 5
+let multiply = (num1 * num2)
+let random = Math.floor(Math.random() + 1)
+let mod = (num3 % num4)
+let max = Math.max(1, 11, 12,20)
